88c0231aad534b126d094198d845a5960e33066f
brenthompson2 <brenthompson2@gmail.com>
2018-02-12 23:21:09 -0500

    Worked on Alarm and set App Name

--- app/src/main/java/com/brendansapps/soulmeds/alarmsActivity.java
+++ app/src/main/java/com/brendansapps/soulmeds/alarmsActivity.java
@@ -2,7 +2,6 @@ package com.brendansapps.soulmeds;
 
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,9 +11,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import android.widget.Button;
-import android.widget.TimePicker;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +18,6 @@ public class alarmsActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter; // Manages the Pages in Memory
     private ViewPager mViewPager; // Hosts the Section Contents
-    private Button mSetAlarmButton;
-    private TimePicker mTimePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,61 +36,16 @@ public class alarmsActivity extends AppCompatActivity {
         tabLayout.getTabAt(1).setText("2");
         tabLayout.getTabAt(2).setText("3");
 
-        // Find the Alarm Buttons
-        mSetAlarmButton = findViewById(R.id.setAlarmBtn1);
-        mTimePicker = findViewById(R.id.alarmTimePicker1);
-
-//      mSetAlarmButton.setOnClickListener(new View.OnClickListener() {
-//           @Override
-//           public void onClick(View v) {
-//               Calendar calendar = Calendar.getInstance();
-//
-//               if (Build.VERSION.SDK_INT >= 23) {
-//                   calendar.set(
-//                           calendar.get(Calendar.YEAR),
-//                           calendar.get(Calendar.MONTH),
-//                           calendar.get(Calendar.DAY_OF_YEAR),
-//                           mTimePicker.getHour(),
-//                           mTimePicker.getMinute(),
-//                           0
-//                   );
-//               } else {
-//                   calendar.set(
-//                           calendar.get(Calendar.YEAR),
-//                           calendar.get(Calendar.MONTH),
-//                           calendar.get(Calendar.DAY_OF_YEAR),
-//                           mTimePicker.getCurrentHour(),
-//                           mTimePicker.getCurrentMinute(),
-//                           0
-//                   );
-//
-//               }
-//
-//               setAlarm(calendar.getTimeInMillis());
-//           }
-//        });
-
     }
 
+    // Adds the fragments to the View Page / SectionsPagerAdapter / FragmentList
     private void setupViewPager(ViewPager viewPager) {
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new alarms_fragment1());
         adapter.addFragment(new alarms_fragment2());
         adapter.addFragment(new alarms_fragment3());
         viewPager.setAdapter(adapter);
-
     }
-
-    private void setAlarm(long timeInMillis) {
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-//        Intent intent = new Intent(this, MyAlarm.class);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
-//
-//        Toast.makeText(this, "Alarm is Set", Toast.LENGTH_SHORT).show();
-
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -120,7 +69,7 @@ public class alarmsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // Fragment Manager
+    // Fragment Manager Class
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         // Create a List of fragments
--- app/src/main/java/com/brendansapps/soulmeds/alarms_fragment1.java
+++ app/src/main/java/com/brendansapps/soulmeds/alarms_fragment1.java
@@ -1,5 +1,11 @@
 package com.brendansapps.soulmeds;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +13,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by bt on 2/12/18.
@@ -16,14 +26,63 @@ public class alarms_fragment1 extends Fragment {
     private static final String TAG = "alarms_fragment1";
 
     private Button mAlarmButton;
+    private TimePicker mTimePicker;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment1_alarms, container, false);
+
+        // Find the Alarm
         mAlarmButton = view.findViewById(R.id.setAlarmBtn1);
+        mTimePicker = view.findViewById(R.id.alarmTimePicker1);
+
+      mAlarmButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Calendar calendar = Calendar.getInstance();
+
+               if (Build.VERSION.SDK_INT >= 23) {
+                   calendar.set(
+                           calendar.get(Calendar.YEAR),
+                           calendar.get(Calendar.MONTH),
+                           calendar.get(Calendar.DAY_OF_YEAR),
+                           mTimePicker.getHour(),
+                           mTimePicker.getMinute(),
+                           0
+                   );
+               }
+               else {
+                   calendar.set(
+                           calendar.get(Calendar.YEAR),
+                           calendar.get(Calendar.MONTH),
+                           calendar.get(Calendar.DAY_OF_YEAR),
+                           mTimePicker.getCurrentHour(),
+                           mTimePicker.getCurrentMinute(),
+                           0
+                   );
+               }
+
+               setAlarm(calendar.getTimeInMillis());
+           }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    private void setAlarm(long timeInMillis) {
+        Context context = this.getContext();
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, MyAlarm.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        Toast.makeText(context, "Alarm is Set", Toast.LENGTH_SHORT).show();
     }
 
 
