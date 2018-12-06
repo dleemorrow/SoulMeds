5aa3d68d0a019b7ba3a4144ba79b7c48ad1584d5
David Morrow <d.lee.morrow@gmail.com>
2018-12-05 20:36:31 -0500

    Fixed null entry crashes on password reset

--- app/src/main/java/com/brendansapps/soulmeds/ResetActivity.java
+++ app/src/main/java/com/brendansapps/soulmeds/ResetActivity.java
@@ -3,6 +3,7 @@ package com.brendansapps.soulmeds;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
@@ -38,17 +39,46 @@ public class ResetActivity extends AppCompatActivity {
     }
 
     private void sendReset(){
+
         String email = mEmailView.getText().toString();
-        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "Email sent.");
-                            Toast.makeText(getApplicationContext(), "Email Sent", Toast.LENGTH_SHORT).show();
-                            finish();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Email sent.");
+                                Toast.makeText(getApplicationContext(), "Email sent", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Account not found", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
     }
 }
